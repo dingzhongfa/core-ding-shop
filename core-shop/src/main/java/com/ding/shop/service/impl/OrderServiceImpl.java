65b2421605511bf444e046bd2bb371618e78b38c
ding <ding_1995_7@163.com>
2017-01-18 18:00:10 +0800

    Spring搭建完成

--- core-shop/src/main/java/com/ding/shop/service/impl/OrderServiceImpl.java
+++ core-shop/src/main/java/com/ding/shop/service/impl/OrderServiceImpl.java
@@ -5,8 +5,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Service;
 
 /**
+ *
  * Created by hzdzf on 2017/1/13.
  */
-@Service
+
 public class OrderServiceImpl implements OrderService {
 }
--- core-shop/src/main/java/com/ding/shop/service/impl/UserServiceImpl.java
+++ core-shop/src/main/java/com/ding/shop/service/impl/UserServiceImpl.java
@@ -5,9 +5,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Service;
 
 /**
+ *
  * Created by hzdzf on 2017/1/13.
  */
-@Service
+
 public class UserServiceImpl implements UserService {
 
 }
